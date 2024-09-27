eca9d22e452a5f9cef0316972d561af7639e0f20
khi8194 <khi8194@gmail.com>
2024-09-27 18:00:15 +0900

    브라우저 리사이즈시 1000px 넘어가는 순간 강제로 모바일패널 닫기

--- js/common.js
+++ js/common.js
@@ -4,3 +4,11 @@ const mobilePanel = document.querySelector(".mobilePanel");
 btnMo.addEventListener("click", () => {
 	mobilePanel.classList.toggle("on");
 });
+
+//브라우저 리사이즈시 브라우저폭의 너비가 1000px 이상일 때 강제로 mobilePanel의 left값을 -340px로 변경
+window.addEventListener("resize", () => {
+	const wid = window.innerWidth;
+	console.log(wid);
+
+	if (wid >= 1000) mobilePanel.classList.remove("on");
+});
